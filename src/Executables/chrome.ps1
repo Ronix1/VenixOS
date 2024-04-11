95ca648fa93e23dd2e03350b54060c765f42ad4a
Ronix1 <165508755+Ronix1@users.noreply.github.com>
2024-04-12 01:03:47 +0200

    Delete src directory

--- src/Executables/chrome.ps1
+++
@@ -1,8 +0,0 @@
-$tempDir = Join-Path -Path $env:TEMP -ChildPath $([System.Guid]::NewGuid())
-New-Item $tempDir -ItemType Directory -Force | Out-Null
-Push-Location $tempDir
-
-curl.exe -LSs "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -o "$tempDir\chrome.exe"
-
-
-cmd /c "$tempDir\chrome.exe" /silent /install
--- src/Executables/dns.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: set primary dns server to 1.1.1.1 
-netsh interface ip set dns name="Ethernet" static 1.1.1.1 primary >nul 2>&1
-
-:: set secondary dns server to 1.0.0.1
-netsh interface ip add dns name="Ethernet" addr=1.0.0.1 index=2 >nul 2>&1
