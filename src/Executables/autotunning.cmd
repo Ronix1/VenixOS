b223cf2831a4e0bce632cd8e76ffcb939c2b6a41
Ronix1 <165508755+Ronix1@users.noreply.github.com>
2024-04-12 00:44:51 +0200

    Delete src directory

--- src/Executables/autotunning.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-:: enable autotunning
-netsh int tcp set global autotuninglevel=normal >nul 2>&1
--- src/Executables/disable-nagles-algorithm.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-reg add "HKLM\SOFTWARE\Microsoft\MSMQ\Parameters" /v "TCPNoDelay" /t REG_DWORD /d "1" /f >nul 2>&1  
-for /f "tokens=3*" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkCards" /f "ServiceName" /s ^|findstr /i /l "ServiceName"') do (
-		reg add "HKLM\System\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%i" /v "TCPNoDelay" /t REG_DWORD /d "1" /f
-		reg add "HKLM\System\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%i" /v "TcpAckFrequency" /t REG_DWORD /d "1" /f
-		reg add "HKLM\System\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%i" /v "TcpDelAckTicks" /t REG_DWORD /d "0" /f
-) >nul 2>&1
